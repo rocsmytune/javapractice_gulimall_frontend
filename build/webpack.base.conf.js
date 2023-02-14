9d0ef09cdc78c9c97d9ae7acd7f5bbc9591f2672
Mingzhe <m.shi@student.tue.nl>
2023-02-14 01:56:07 +0100

    #optimize: close vue-format check, add scoped-slot

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,14 +9,15 @@ function resolve (dir) {
 }
 
 const createLintingRule = () => ({
-  test: /\.(js|vue)$/,
-  loader: 'eslint-loader',
-  enforce: 'pre',
-  include: [resolve('src'), resolve('test')],
-  options: {
-    formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
-  }
+  // test: /\.(js|vue)$/,
+  // loader: 'eslint-loader',
+  // enforce: 'pre',
+  // include: [resolve('src'), resolve('test')],
+  // options: {
+  //   formatter: require('eslint-friendly-formatter'),
+  //   emitWarning: !config.dev.showEslintErrorsInOverlay
+  // }
+  //暂时删除严格的语法检查
 })
 
 module.exports = {
